c4821744fe5bb2d7d543e806197f19c17d3ac12c
wangzivector <1664179213@qq.com>
2020-05-13 10:32:34 +0800

    update gui_node.cpp

--- src/qt_ros_pcl/gui/gui_node.cpp
+++ src/qt_ros_pcl/gui/gui_node.cpp
@@ -1,3 +1,10 @@
+/*
+ * gui_node.cpp
+ *
+ *  Created on: May 8, 2020
+ *      Author: wangzivector
+ * This cppfile handle the gui setting
+ */
 #include "mainwindow.h"
 #include <QApplication>
 #include <gui_node.h>
